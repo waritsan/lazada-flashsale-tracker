4a4c24aa6a61c4a3ab22fd5ebe597036c1cc285b
Warit Santaputra <waritsan@gmail.com>
2019-05-18 18:36:00 +0700

    Extract item names from flash sale

--- getFlashSale.js
+++ getFlashSale.js
@@ -1,20 +1,26 @@
 const puppeteer = require('puppeteer')
+const cheerio = require('cheerio')
 const fs = require('fs')
 
 const uri = 'https://pages.lazada.co.th/wow/i/th/LandingPage/flashsale'
 
 async function getFlashSale() {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.goto(uri, { waitUntil: 'networkidle2' })
-  const html = await page.content()
-  fs.writeFileSync('flash-sale.html', html)
-  await browser.close()
-
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto(uri, { waitUntil: 'networkidle2' })
+    const html = await page.content()
+    await browser.close()
+    return html
 }
 
 async function lookForItems() {
-  // TODO
+    const data = await getFlashSale()
+    const $ = cheerio.load(data)
+    const saleTitleElements = $('div .sale-title')
+    saleTitleElements.each((index, saleTitleElement) => {
+        const name = $(saleTitleElement).text()
+        console.log(name)
+    })
 }
 
-getFlashSale()
+lookForItems()
