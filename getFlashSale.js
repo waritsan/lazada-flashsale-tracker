2d16524a4ebc1bf48c1aa70eae536a81f0396035
Warit Santaputra <waritsan@gmail.com>
2019-05-18 21:19:38 +0700

    Fix deployment issue

--- getFlashSale.js
+++ getFlashSale.js
@@ -5,7 +5,9 @@ const fs = require('fs')
 const uri = 'https://pages.lazada.co.th/wow/i/th/LandingPage/flashsale'
 
 async function getFlashSale() {
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    })
     const page = await browser.newPage()
     await page.goto(uri, { waitUntil: 'networkidle2' })
     const html = await page.content()
